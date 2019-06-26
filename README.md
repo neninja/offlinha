93ee8cce87d486bb3ac448c556801a1573bdc620
Felipe Silva <felipe.dacs@gmail.com>
2019-06-26 15:41:55 -0300

    :tada: 2

---
+++ README.md
@@ -0,0 +1,29 @@
+# offlinha
+PWA com os horários de alguns ônibus de Porto Alegre/RS.
+
+## Project
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Deploy
+```
+npm run deploy
+```
+## Metas pessoais de desenvolvimento
+* [x] Usar gulp para build e hot-reload com browser-sync
+* [x] Não utilizar frameworks/bibliotecas externas além do ambiente de desenvolvimento
+* [x] Deploy automatizado
+
+## Metas do projeto
+* [ ] Identificar qual época do ano exibir os horários
+* [ ] Permitir que sejam feitas anotações nos horários
+* [ ] Permitir que possa ser visualizado "qual lado possui sol no ônibus"
+* [ ] Permitir exportar anotações e "lado de sol do onibus"
+* [ ] Permitir importar anotações e "lado de sol do onibus"
+
---
+++ service-worker.js
@@ -0,0 +1,42 @@
+let cacheName = 'offlinha-v.1.0.0';
+let filesToCache = [
+    './',
+    'index.html',
+    'index.css',
+    'index.js',
+    'store.js'
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installer');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
