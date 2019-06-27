cdaf7bab8b2195d8b9e4b8eccdcdf2d396507cc7
Felipe Silva <felipe.dacs@gmail.com>
2019-06-27 15:09:41 -0300

    Estratégia network first

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,53 @@
+// https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
 let cacheName = 'offlinha-v.1.0.0';
 let filesToCache = [
-    './',
-    'index.html',
-    'index.css',
-    'index.js',
-    'store.js'
+	'./',
+	'index.html',
+	'index.css',
+	'index.js',
+	'store.js'
 ];
 
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Installer');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
-    );
+	console.log('[ServiceWorker] Installer');
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			console.log('[ServiceWorker] Caching app shell');
+			return cache.addAll(filesToCache);
+		})
+	);
 });
 
 self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+	console.log('[ServiceWorker] Activate');
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key !== cacheName) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+// Network first
+self.addEventListener('fetch', function(e) {
+console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    fetch(e.request).catch(function() {
+        console.log('[ServiceWorker] Fetch offline', e.request.url);
+      return caches.match(e.request);
+    })
+  );
 });
 
+// Cache first
+// self.addEventListener('fetch', function (e) {
+// console.log('[ServiceWorker] Fetch', e.request.url);
+// e.respondWith(
+// // caches.match(e.request).then(function(response) {
+// // return response || fetch(e.request);
+// // })
+// });
